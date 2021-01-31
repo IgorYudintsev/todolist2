332f87c4e5227ec9e89044e5a34d57e561834118
mobile08092018 <kakvospitat@gmail.com>
2021-01-31 17:13:31 +0300

    13.3

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -1,30 +1,35 @@
 import axios from "axios";
 
-const settings = {
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
         'API-KEY': '0e5dc50f-7e9f-4eda-9157-a63c5026aaad'
     }
-}
+})
+
+
+// const settings = {
+//     withCredentials: true,
+//     headers: {
+//         'API-KEY': '0e5dc50f-7e9f-4eda-9157-a63c5026aaad'
+//     }
+// }
 let todolistIDtoDelete = '9b105ec2-5aa5-4edd-a0ee-31d8e505f6c2';
 let todolistIDtoUpdate = 'c831a55a-10f0-4258-87a0-b21b55e7404b';
 
 export const todoApi = {
     getTodos() {
-        let promis = axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings);
-        return promis
+        return instance.get('todo-lists');
     },
     createTodos() {
-        let promis = axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title: 'React'}, settings)
-        return promis
+        return instance.post('todo-lists', {title: 'React'})
     },
     DeleteTodos() {
-        let promis = axios.delete(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistIDtoDelete}`, settings)
-        return promis
+        return instance.delete(`todo-lists/${todolistIDtoDelete}`)
     },
     UpdateTodos() {
-        let promis = axios.put(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todolistIDtoUpdate}`, {title: 'Redux'}, settings)
-        return promis
+        return instance.put(`todo-lists/${todolistIDtoUpdate}`, {title: 'Redux'})
     }
 }
 
